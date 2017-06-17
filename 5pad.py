fa78b6b796dccc31b685b0c8fb597038249bc1e9
kat austen <iamkat@gmail.com>
2017-06-17 19:15:22 +0200

    Main files for the set up of the 5pad sensor

---
+++ 5pad.py
@@ -0,0 +1,59 @@
+import RPi.GPIO as GPIO
+import subprocess
+import time
+ 
+GPIO.setmode(GPIO.BCM)
+ 
+#set the GPIO input pins
+pad0 = 22
+pad1 = 27
+pad2 = 17
+pad3 = 24
+pad4 = 23
+ 
+GPIO.setup(pad0, GPIO.IN)
+GPIO.setup(pad1, GPIO.IN)
+GPIO.setup(pad2, GPIO.IN)
+GPIO.setup(pad3, GPIO.IN)
+GPIO.setup(pad4, GPIO.IN)
+ 
+pad0alreadyPressed = False
+pad1alreadyPressed = False
+pad2alreadyPressed = False
+pad3alreadyPressed = False
+pad4alreadyPressed = False
+ 
+ 
+while True:
+    pad0pressed = not GPIO.input(pad0)
+    pad1pressed = not GPIO.input(pad1)
+    pad2pressed = not GPIO.input(pad2)
+    pad3pressed = not GPIO.input(pad3)
+    pad4pressed = not GPIO.input(pad4)
+    
+    if pad0pressed and not pad0alreadyPressed:
+        print "Pad 0 pressed"
+   	subprocess.call(['/home/pi/touch_pad/rungif.sh']) 
+    pad0alreadyPressed = pad0pressed
+ 
+    if pad1pressed and not pad1alreadyPressed:
+        print "Pad 1 pressed"
+	subprocess.call(['/home/pi/touch_pad/rungif.sh']) 
+    pad1alreadyPressed = pad1pressed
+ 
+    if pad2pressed and not pad2alreadyPressed:
+        print "Pad 2 pressed"
+	subprocess.call(['/home/pi/touch_pad/rungif.sh']) 
+    pad2alreadyPressed = pad2pressed
+ 
+    if pad3pressed and not pad3alreadyPressed:
+        print "Pad 3 pressed"
+	subprocess.call(['/home/pi/touch_pad/rungif.sh']) 
+    pad3alreadyPressed = pad3pressed
+ 
+    if pad4pressed and not pad4alreadyPressed:
+        print "Pad 4 pressed"
+	subprocess.call(['/home/pi/touch_pad/rungif.sh']) 
+    pad4alreadyPressed = pad4pressed
+ 
+    time.sleep(0.1)
---
+++ rungif.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#runs omxplayer to play gif on loop
+/usr/bin/omxplayer --no-osd /home/pi/touch_pad/giphy.mp4 
